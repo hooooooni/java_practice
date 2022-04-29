27d49c2d069e3acdfe40730ae7028ff5d4842915
hooooooni <kheee2010@dgu.ac.kr>
2022-04-29 21:30:58 +0900

    CharacterEx

---
+++ CharacterEx2.java
@@ -0,0 +1,9 @@
+public class CharacterEx2 {
+    public static void main(String[ ] args) {
+        char ch1 = '한';
+        char ch2 = '\uD55C'; //'한'이라는 글자의 유니코드 값, 16진수로 나타냄
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+    }
+}
